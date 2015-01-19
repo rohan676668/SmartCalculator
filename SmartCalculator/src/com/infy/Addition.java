a6ef5e9747a34b3cc6bb417e2f6fffdd202348fd
rohan <rohan@abc,com>
2015-01-19 06:32:54 +0530

    done

--- SmartCalculator/src/com/infy/Addition.java
+++ SmartCalculator/src/com/infy/Addition.java
@@ -3,7 +3,10 @@ package com.infy;
 public class Addition {
 	
 	public static int add(int first,int second){
+	 
+		
 		System.out.println("Add");
+		System.out.println("Add11111");
 		 return first+second;
 }
 }
