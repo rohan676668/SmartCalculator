37bdc4303c03af0618e270050834a855a8c98909
rohan <rohan@abc,com>
2015-01-19 06:02:32 +0530

    added noo

--- SmartCalculator/src/com/infy/Main.java
+++ SmartCalculator/src/com/infy/Main.java
@@ -10,6 +10,8 @@ public class Main {
 		System.out.println("Addtion of 10 and 30= "+Addition.add(10, 30));
 		System.out.println("Subtraction of 30 and 10= "+Subtraction.sub(30,10));
 		System.out.println("OKK");
+		System.out.println("OKKNAzzz");
+		
 	}
 
 }
